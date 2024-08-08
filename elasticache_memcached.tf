34cfa9835d8b344584f163c58acd9821f73bdc33
Sourav Kundu <skundu.dev@gmail.com>
2024-08-07 23:28:38 -0500

    added elasticache for #3

---
+++ elasticache_memcached.tf
@@ -0,0 +1,9 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster
+resource "aws_elasticache_cluster" "cache_cluster" {
+  cluster_id           = var.name
+  engine               = "memcached"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 2
+  parameter_group_name = "default.memcached1.4"
+  port                 = 11211
+}
