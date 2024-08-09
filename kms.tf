21770939626922a595530a88ed42df1120dd1e9c
Sourav Kundu <skundu.dev@gmail.com>
2024-08-09 14:12:15 -0500

    #3 added endpoint details

---
+++ kms.tf
@@ -0,0 +1,54 @@
+resource "aws_kms_key" "encrypt_ssm" {
+  enable_key_rotation     = true
+  description             = "Key to encrypt ssm"
+  deletion_window_in_days = 7
+  #checkov:skip=CKV2_AWS_64: Not including a KMS Key policy
+}
+resource "aws_kms_alias" "encryption_secret" {
+  name          = "alias/${var.name}"
+  target_key_id = aws_kms_key.encrypt_ssm.key_id
+}
+resource "aws_kms_key_policy" "encrypt_ssm_policy" {
+  key_id = aws_kms_key.encrypt_ssm.id
+  policy = data.aws_iam_policy_document.encrypt_ssm_policy.json
+}
+
+data "aws_iam_policy_document" "encrypt_ssm_policy" {
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow SSM to use the key"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ssm.amazonaws.com"]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "kms:CallerAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+      values   = ["ssm.${data.aws_region.current.name}.amazonaws.com"]
+    }
+  }
+}
--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -1,6 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ssm_parameter
 resource "aws_ssm_parameter" "elasticache_ep" {
-  name  = "/elasticache/${var.name}/endpoint"
-  type  = "SecureString"
-  value = aws_elasticache_cluster.cache_cluster.configuration_endpoint
+  name   = "/${var.name}/endpoint"
+  type   = "SecureString"
+  key_id = aws_kms_key.encrypt_ssm.id
+  value  = aws_elasticache_cluster.cache_cluster.configuration_endpoint
 }
