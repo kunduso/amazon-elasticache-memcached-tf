b9b57c1000573c6032bfb387900d36e0353a9c9e
Sourav Kundu <skundu.dev@gmail.com>
2024-08-15 09:49:56 -0500

    format

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 
 resource "aws_vpc" "this" {
-  #checkov:skip=CKV2_AWS_11: This is non prod and hence disabled.
+  # checkov:skip=CKV2_AWS_11: This is non prod and hence disabled.
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
