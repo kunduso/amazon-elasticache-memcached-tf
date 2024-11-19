b94a9ded40dbb83cd0dd8e7e4c014526e284f117
Sourav Kundu <skundu.dev@gmail.com>
2024-11-19 13:34:56 -0600

    corrected vpc_name to variable

--- network.tf
+++ network.tf
@@ -6,7 +6,7 @@ module "vpc" {
   vpc_cidr                = var.vpc_cidr
   enable_dns_support      = "true"
   enable_dns_hostnames    = "true"
-  vpc_name                = "app-4"
+  vpc_name                = var.name
   subnet_cidr_private     = var.subnet_cidr_private
   subnet_cidr_public      = var.subnet_cidr_public
   enable_flow_log         = "true"
